4422721896bedbdbcdcf321b67e079481a02c331
Martin Tverraen <martin.tverraen@evry.com>
2020-08-03 22:50:53 +0200

    update run-script

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import subprocess
 
-subprocess.Popen("python3 tk1-recovery.py 25 0.3 250", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 25 0.5 250", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 25 0.7 250", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 25 0.9 250", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 25 0.3 100", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 25 0.5 100", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 25 0.7 100", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 25 0.9 100", shell=True)
 
 
 
