b76c500ee65f3d8d32e77f767769efdeee8828ec
Martin Tverraen <martin.tverraen@evry.com>
2020-08-05 06:41:40 +0200

    required traces datafiles

--- plot_2.py
+++ plot_2.py
@@ -5,9 +5,13 @@ import matplotlib.pyplot as plt
 import math
 import sys
 
-std=[0.1,0.3,0.5,0.7,0.9]
+std=[0.1,0.3,0.5,0.7]
 requires_traces_for_s_rate=[]
-for s in range(std):
+for s in std:
     infile="requires_traces_"+str(s)+".npy"
     traces=np.load(infile)
     requires_traces_for_s_rate.append(traces)
+
+for row in requires_traces_for_s_rate:
+    print(row)
+
