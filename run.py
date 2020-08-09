1c23a54ccbccbc7432e4a35e1980005999430792
Martin T <martin.tverraen@evry.com>
2020-08-09 12:36:04 +0000

    plots

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import subprocess
 
-subprocess.Popen("python3 tk1-recovery.py 20 0.3 150", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 20 0.5 150", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 20 0.7 150", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 20 0.9 150", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 20 0.3 250", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 20 0.5 250", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 20 0.7 250", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 20 0.9 250", shell=True)
 
 
 
