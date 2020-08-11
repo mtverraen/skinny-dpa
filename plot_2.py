bb67dbede1bbfebc8e233837d06da5ad53a8b945
Martin Tverraen <martin.tverraen@evry.com>
2020-08-11 23:10:36 +0200

    fix plotting section

--- plot_2.py
+++ plot_2.py
@@ -7,8 +7,10 @@ import math
 import sys
 from numpy import arange
 
-#std=[0.1,0.3,0.5,0.7]
-std=arange(0.1, 1.1, 0.05)
+#std=[0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1.0,1.05]
+std=arange(0.1, 1.5, 0.05)
+print(std)
+
 requires_traces_for_s_rate=[]
 for s in std:
     infile="requires_traces_"+str(s)+".npy"
@@ -17,15 +19,25 @@ for s in std:
 
 
 req_t =np.matrix(requires_traces_for_s_rate)
+print(req_t)
 
-simultanous=req_t[:,0]
-majority_vote=req_t[:,1]
-unanimous=req_t[:,2]
-individual=req_t[:,3]
+simultanous=req_t[:,0].A1
+majority_vote=req_t[:,1].A1
+unanimous=req_t[:,2].A1
+individual=req_t[:,3].A1
 outfile= "required_traces_pr_sigma"
-helpers.plot_required_traces(simultanous, majority_vote, 
-                 unanimous, individual,
-                 "simultanous","majority vote",
-                 "unanimous","individual",
-                outfile,std)
+
+print(simultanous)
+plt.figure()
+plt.ylabel('Traces')
+plt.xlabel('σ')
+plt.plot(std,np.array(simultanous), label="simultanous", c="red")
+plt.plot(std,np.array(majority_vote), label="majority vote", c="blue")
+plt.plot(std,np.array(unanimous), label="unanimous", c="green")
+plt.plot(std,np.array(individual), label="individual", c="black")
+plt.grid(axis='y')
+#plt.gca().xaxis.set_major_formatter(StrMethodFormatter('{x:,.0f}')) # No decimal places
+plt.legend()
+plt.draw()
+plt.savefig(outfile)
 
