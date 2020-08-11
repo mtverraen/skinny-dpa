fdf08807a752481e8648eb0c599fb75c993548c4
Martin T <martin.tverraen@evry.com>
2020-08-11 10:21:24 +0000

    more data

--- run.py
+++ run.py
@@ -1,16 +1,6 @@
 import subprocess
 
-<<<<<<< HEAD
-subprocess.Popen("python3 tk1-recovery.py 20 0.3 250", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 20 0.5 250", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 20 0.7 250", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 20 0.9 250", shell=True)
-=======
-subprocess.Popen("python3 tk1-recovery.py 25 0.3 200", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 25 0.5 200", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 25 0.7 200", shell=True)
-subprocess.Popen("python3 tk1-recovery.py 25 0.9 200", shell=True)
->>>>>>> b76c500ee65f3d8d32e77f767769efdeee8828ec
-
-
-
+subprocess.Popen("python3 tk1-recovery.py 25 0.3 500", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 25 0.5 500", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 25 0.7 500", shell=True)
+subprocess.Popen("python3 tk1-recovery.py 25 0.9 500", shell=True)
